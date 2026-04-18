59f5bff8011f54c1262cf361f519a388a89538fe
Nusrat Jahan <nusrat.jahan.az@gmail.com>
2026-04-18 18:45:36 +0600

    Add cloudinary due to render free tier memory limit

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,52 +1,22 @@
-// export default [
-//   "strapi::logger",
-//   "strapi::errors",
-//   {
-//     name: "strapi::security",
-//     config: {
-//       contentSecurityPolicy: {
-//         useDefaults: true,
-//         directives: {
-//           "connect-src": ["'self'", "https:"],
-//           "img-src": [
-//             "'self'",
-//             "data:",
-//             "blob:",
-//             "res.cloudinary.com",
-//             "mirsaige-real-state-backend.onrender.com",
-//           ],
-//           "media-src": ["'self'", "data:", "blob:"],
-//           upgradeInsecureRequests: null,
-//         },
-//       },
-//     },
-//   },
-//   {
-//     name: "strapi::cors",
-//     config: {
-//       origin: [
-//         "http://localhost:3000",
-//         "https://mirsaige-real-state-frontend.vercel.app",
-//       ],
-//       methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"],
-//       headers: ["Content-Type", "Authorization", "Origin", "Accept"],
-//       keepHeaderOnError: true,
-//     },
-//   },
-//   "strapi::poweredBy",
-//   "strapi::query",
-//   "strapi::body",
-//   "strapi::session",
-//   "strapi::favicon",
-//   "strapi::public",
-// ];
-
 import type { Core } from "@strapi/strapi";
 
 const config: Core.Config.Middlewares = [
   "strapi::logger",
   "strapi::errors",
-  "strapi::security",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
+          "media-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
 
   {
     name: "strapi::cors",
