30a7548c3542ae6ab7edf6f7deb7d22203e9795b
Nusrat Jahan <nusrat.jahan.az@gmail.com>
2026-04-18 17:35:07 +0600

    Checking the cors issue

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,38 +1,62 @@
-export default [
+// export default [
+//   "strapi::logger",
+//   "strapi::errors",
+//   {
+//     name: "strapi::security",
+//     config: {
+//       contentSecurityPolicy: {
+//         useDefaults: true,
+//         directives: {
+//           "connect-src": ["'self'", "https:"],
+//           "img-src": [
+//             "'self'",
+//             "data:",
+//             "blob:",
+//             "res.cloudinary.com",
+//             "mirsaige-real-state-backend.onrender.com",
+//           ],
+//           "media-src": ["'self'", "data:", "blob:"],
+//           upgradeInsecureRequests: null,
+//         },
+//       },
+//     },
+//   },
+//   {
+//     name: "strapi::cors",
+//     config: {
+//       origin: [
+//         "http://localhost:3000",
+//         "https://mirsaige-real-state-frontend.vercel.app",
+//       ],
+//       methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"],
+//       headers: ["Content-Type", "Authorization", "Origin", "Accept"],
+//       keepHeaderOnError: true,
+//     },
+//   },
+//   "strapi::poweredBy",
+//   "strapi::query",
+//   "strapi::body",
+//   "strapi::session",
+//   "strapi::favicon",
+//   "strapi::public",
+// ];
+
+import type { Core } from "@strapi/strapi";
+
+const config: Core.Config.Middlewares = [
   "strapi::logger",
   "strapi::errors",
-  {
-    name: "strapi::security",
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          "connect-src": ["'self'", "https:"],
-          "img-src": [
-            "'self'",
-            "data:",
-            "blob:",
-            "res.cloudinary.com",
-            "mirsaige-real-state-backend.onrender.com",
-          ],
-          "media-src": ["'self'", "data:", "blob:"],
-          upgradeInsecureRequests: null,
-        },
-      },
-    },
-  },
+  "strapi::security",
+
   {
     name: "strapi::cors",
     config: {
-      origin: [
-        "http://localhost:3000",
-        "https://mirsaige-real-state-frontend.vercel.app",
-      ],
-      methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"],
-      headers: ["Content-Type", "Authorization", "Origin", "Accept"],
-      keepHeaderOnError: true,
+      origin: ["http://localhost:3000", "https://your-frontend.vercel.app"],
+      methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+      headers: ["Content-Type", "Authorization"],
     },
   },
+
   "strapi::poweredBy",
   "strapi::query",
   "strapi::body",
@@ -40,3 +64,5 @@ export default [
   "strapi::favicon",
   "strapi::public",
 ];
+
+export default config;
